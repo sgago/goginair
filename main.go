a011b01cc69c85a77d49dce0bf4549adb3f9451c
Steven Gago <steven.a.gago@gmail.com>
2024-04-12 19:01:28 -0500

    checkpoint

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"embed"
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	//go:embed templates
+	embeddedFiles embed.FS
+)
+
+const (
+	templPath = "templates/*"
+
+	indexPath  = "/"
+	indexTempl = "index.tmpl"
+
+	healthzPath = "/healthz"
+)
+
+func main() {
+	templ := template.
+		Must(template.New("").
+			ParseFS(embeddedFiles, templPath))
+
+	r := gin.Default()
+
+	r.SetHTMLTemplate(templ)
+
+	r.GET(healthzPath, healthz)
+	r.GET(indexPath, index)
+
+	r.Run()
+}
+
+func index(c *gin.Context) {
+	c.HTML(200, indexTempl, gin.H{
+		"title": "GoGinAir",
+	})
+}
+
+func healthz(c *gin.Context) {
+	c.Writer.WriteHeader(http.StatusOK)
+}
